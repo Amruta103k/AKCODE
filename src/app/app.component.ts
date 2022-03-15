60447c8a5ee14f38098c0ae0b88907637a132026
Amruta@git10 <kasar.amruta103@gmail.com>
2022-03-15 15:18:26 +0530

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'MyFirstApp';
+  owner ='Ms.Amruta Kasar Shewale';
+ fname='';
+lname='';
+ngOnInit(){
+    this.fname='Rahul',
+this.lname='Shewale'
+  }
 }
